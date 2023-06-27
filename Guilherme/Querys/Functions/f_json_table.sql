2a5e951f6ca311a1da42cdab6201c749abd7b13b
guilherme-dr-online <guilhermeinttegrar@gmail.com>
2023-06-27 10:20:06 -0300

    função para multi seleção

---
+++ Guilherme/Querys/Functions/f_json_table.sql
@@ -0,0 +1,23 @@
+-- para adiciona a função f_jason devemos utilizar os seguinte parametros 
+"[\"98\", \"1\"]" -- as barras \ farão o dbever esquecer [] chave de forma que ele possa executar mais de 1 parametro para a condição
+-- lembrando que para usar essa função devemos utilizar o in em uma condição wherer
+
+
+--exemplo
+	select
+	DISTINCT
+		p.idpessoa_fisica p_idpessoa,
+		upper(f_profissional_dados(p.idprofissional , 'NC')) nome,
+		CONCAT(f_profissional_dados(p.idprofissional, 'SCP'), ': ', f_profissional_dados(p.idprofissional, 'NCP'), ' - ', f_profissional_dados(p.idprofissional, 'UF')) conselho
+from
+	atendimento_paciente ap
+inner join classificacao_atendimento ca on
+	ca.idclassificacao_atendimento = ap.idclassificacao
+inner join profissional p on
+	p.idpessoa_fisica = ap.idmedico_atendimento
+	and p.idestab_orig = ap.idestabelecimento
+where
+	DATE_FORMAT(dt_inicio_atendimento, '%Y-%m-%d') BETWEEN :dt_inicio and COALESCE(:dt_fim, CURRENT_DATE())
+	and(ap.ie_status_atendimento in(f_json_table(:status,ap.ie_status_atendimento))or :status is NULL);
+	
+-- use o seguinte parametro em status = "[\"C\",\"A\"]"
