9b7382ae03d90de49384b71536b7ca24dec7f3ba
guilherme-dr-online <guilhermeinttegrar@gmail.com>
2023-07-03 11:00:05 -0300

    Alterações do chatbot dr+

--- Guilherme/Chat_bot/chat_dr+.js
+++ Guilherme/Chat_bot/chat_dr+.js
@@ -1,3 +1,4 @@
+//correção
 function horarioFuncionamento() {
     const agora = new Date(Date.now() - 3 * 60 * 60 * 1000);
     const diaDaSemana = agora.getUTCDay();//retorna o dia 
@@ -5,10 +6,22 @@ function horarioFuncionamento() {
     const minutoAtual = agora.getUTCMinutes();//retorna os minutos
   
     const estaDentroHorarioFuncionamento = 
-      (diaDaSemana >= 1 && diaDaSemana <= 5 && horaAtual >= 8 && (horaAtual < 18 || (horaAtual === 18 && minutoAtual <= 00)))//vai funcionar somente apos as 18
+      (diaDaSemana >= 1 && diaDaSemana <= 5 && horaAtual >= 8 && (horaAtual < 18 || (horaAtual === 18 && minutoAtual <= 0)))//vai funcionar somente apos as 18
       || (diaDaSemana === 6 && horaAtual >= 8 && (horaAtual < 12 || (horaAtual === 12 && minutoAtual <= 0)));
    
     return estaDentroHorarioFuncionamento ? "SIM" : "NAO";
   }
 
-//correção
+//original
+function horarioFuncionamento() {
+  const agora = new Date(Date.now() - 3 * 60 * 60 * 1000);
+  const diaDaSemana = agora.getUTCDay();
+  const horaAtual = agora.getUTCHours();
+  const minutoAtual = agora.getUTCMinutes();
+
+  const estaDentroHorarioFuncionamento = 
+    (diaDaSemana >= 1 && diaDaSemana <= 5 && horaAtual >= 8 && (horaAtual < 17 || (horaAtual === 17 && minutoAtual <= 30)))
+    || (diaDaSemana === 6 && horaAtual >= 8 && (horaAtual < 12 || (horaAtual === 12 && minutoAtual <= 0)));
+ 
+  return estaDentroHorarioFuncionamento ? "SIM" : "NAO";
+}
