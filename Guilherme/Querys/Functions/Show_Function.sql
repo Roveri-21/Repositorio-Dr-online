a86953d8e5d4e2abca017d31fddfef4374c8595b
guilherme-dr-online <guilhermeinttegrar@gmail.com>
2023-07-10 13:19:29 -0300

    consulta de procedure

--- Guilherme/Querys/Functions/Show_Function.sql
+++ Guilherme/Querys/Functions/Show_Function.sql
@@ -6,3 +6,11 @@ where name like '%co%';
 
 -- para consultar o tipo de função 
 SHOW CREATE FUNCTION f_json_table;
+
+
+-- para consultar uma procedure
+SELECT ROUTINE_NAME
+FROM INFORMATION_SCHEMA.ROUTINES
+WHERE ROUTINE_TYPE = 'PROCEDURE'
+    AND ROUTINE_SCHEMA = 'homologacao'
+    AND ROUTINE_NAME like '%p_pivot_dynamic_qtd_lead_etapa_por_origem%';
