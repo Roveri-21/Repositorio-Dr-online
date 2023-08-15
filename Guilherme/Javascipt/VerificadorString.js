e26794a9482858c185af0d43cde82892f09f198e
Roveri-21 <guilherroveri21@gmail.com>
2023-08-15 18:37:11 -0300

    curso de javascripty

---
+++ Guilherme/Javascipt/VerificadorString.js
@@ -0,0 +1,10 @@
+let mensagem = 'ola mundo me chamo guilherme'
+console.log(mensagem.includes('e'))
+//verifica o que ta incluso
+console.log(mensagem.length)
+// conta a quantidade de letras dentro
+console.log(mensagem.startsWith('ola'))
+// verifica se começa com algo ou nao
+console.log(mensagem.endsWith('guilherme'))
+// verifica se termina com algo ou nao
+
