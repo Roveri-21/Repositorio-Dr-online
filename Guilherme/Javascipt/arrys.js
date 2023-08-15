7a6da47150ab0885eb8738dbc7176bc212f554ab
Roveri-21 <guilherroveri21@gmail.com>
2023-08-15 20:20:08 -0300

    curos de js

--- Guilherme/Javascipt/arrys.js
+++ Guilherme/Javascipt/arrys.js
@@ -8,3 +8,5 @@ console.log(amigos[0])
 const numeros = [1,2,3,4,5,6,7,8,9,10];
 console.log(Math.max(...numeros)); // menor numero do arry 
 console.log(Math.min(...numeros)); // maior numero do arry 
+
+
--- Guilherme/Javascipt/if e Else.js
+++ Guilherme/Javascipt/if e Else.js
@@ -9,3 +9,23 @@ else if (drivespeed > 30 && drivespeed < 110){
 else
     console.log('Muito devagar')
 //alteração
+
+
+//function declaration
+function movie(){
+    console.log('the matrix')
+}
+movie()// traz função com o que foi definido dentro dela
+
+// hoisting faz o codigo js trazer a função antes de ser chamada então esse exemplo funcionaria
+
+// movie()
+// function movie(){
+//     console.log('the matrix')
+// }
+
+//function exepression
+const car = function(){
+    console.log('tesla')
+}
+car()// traz o que foi declarado dentro da variavel
