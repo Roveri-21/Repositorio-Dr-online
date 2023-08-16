fa16801476d2debdaa56d3ebf793d73937b2b29d
guilherme-dr-online <guilhermeinttegrar@gmail.com>
2023-08-16 15:49:02 -0300

    Curso

--- Guilherme/Javascipt/arrys.js
+++ Guilherme/Javascipt/arrys.js
@@ -10,3 +10,7 @@ console.log(Math.max(...numeros)); // menor numero do arry
 console.log(Math.min(...numeros)); // maior numero do arry 
 
 
+const num =[7,8,9]
+num.push(10,11,12) // adiciona no final 
+num.unshift(1,2,3) // adiciona no inicio 
+num.splice (3,0,4,5,6) // adiciona no meio porem vc tem que informar onde ela começa e o que ela vai apagar
