897fed5eb3fe0175adbf6a2135d826c0371b532d
guilherme-dr-online <guilhermeinttegrar@gmail.com>
2023-10-02 08:22:21 -0300

    Alteração de logica na query

--- Guilherme/Querys/Relatorios/CRM - Leads por Faixa de Tempo/Detalhe.sql
+++ Guilherme/Querys/Relatorios/CRM - Leads por Faixa de Tempo/Detalhe.sql
@@ -1,28 +1,28 @@
 SELECT 
 	x.idx,
-	x.texto,
-	SUM(x.qts_lead),
-	SUM(x.qtd_andamento),
-	SUM(x.qtd_perdido),
-	SUM(x.qtd_ganho)
+	x.texto as faixa_primeiro_contato,
+	SUM(x.qts_lead) as QTD_LEADS,
+	SUM(x.qtd_andamento)AS EM_ANDAMENTO,
+	SUM(x.qtd_perdido)AS PERDIDAS,
+	SUM(x.qtd_ganho)AS VENDIDAS
 FROM( 
 	SELECT
 		(CASE
 	    WHEN TIMESTAMPDIFF(MINUTE, cl.dt_insert, COALESCE (clh.dt_historico, CURRENT_TIMESTAMP())) <= 30 THEN 1
-	    WHEN TIMESTAMPDIFF(MINUTE, cl.dt_insert, COALESCE (clh.dt_historico, CURRENT_TIMESTAMP())) > 30 AND TIMESTAMPDIFF(MINUTE, cl.dt_insert, clh.dt_historico) < 120 THEN 2
-	    WHEN TIMESTAMPDIFF(HOUR, cl.dt_insert, COALESCE (clh.dt_historico,CURRENT_TIMESTAMP())) >= 2 AND TIMESTAMPDIFF(HOUR, cl.dt_insert, clh.dt_historico) < 8 THEN 3
-	    WHEN TIMESTAMPDIFF(HOUR, cl.dt_insert, COALESCE (clh.dt_historico, CURRENT_TIMESTAMP())) >= 8 AND TIMESTAMPDIFF(HOUR, cl.dt_insert, clh.dt_historico) < 24 THEN 4
-	    WHEN TIMESTAMPDIFF(HOUR, cl.dt_insert, COALESCE (clh.dt_historico, CURRENT_TIMESTAMP())) >= 24 AND TIMESTAMPDIFF(HOUR, cl.dt_insert, clh.dt_historico) < 48 THEN 5
-	    WHEN TIMESTAMPDIFF(HOUR, cl.dt_insert, COALESCE (clh.dt_historico, CURRENT_TIMESTAMP())) >= 48 AND TIMESTAMPDIFF(HOUR, cl.dt_insert, clh.dt_historico) < 72 THEN 6
+	    WHEN TIMESTAMPDIFF(MINUTE, cl.dt_insert, COALESCE (clh.dt_historico, CURRENT_TIMESTAMP())) > 30 AND TIMESTAMPDIFF(MINUTE, cl.dt_insert, COALESCE (clh.dt_historico,CURRENT_TIMESTAMP())) < 120 THEN 2
+	    WHEN TIMESTAMPDIFF(HOUR, cl.dt_insert, COALESCE (clh.dt_historico,CURRENT_TIMESTAMP())) >= 2 AND TIMESTAMPDIFF(HOUR, cl.dt_insert, COALESCE (clh.dt_historico,CURRENT_TIMESTAMP())) < 8 THEN 3
+	    WHEN TIMESTAMPDIFF(HOUR, cl.dt_insert, COALESCE (clh.dt_historico, CURRENT_TIMESTAMP())) >= 8 AND TIMESTAMPDIFF(HOUR, cl.dt_insert, COALESCE (clh.dt_historico,CURRENT_TIMESTAMP())) < 24 THEN 4
+	    WHEN TIMESTAMPDIFF(HOUR, cl.dt_insert, COALESCE (clh.dt_historico, CURRENT_TIMESTAMP())) >= 24 AND TIMESTAMPDIFF(HOUR, cl.dt_insert, COALESCE (clh.dt_historico,CURRENT_TIMESTAMP())) < 48 THEN 5
+	    WHEN TIMESTAMPDIFF(HOUR, cl.dt_insert, COALESCE (clh.dt_historico, CURRENT_TIMESTAMP())) >= 48 AND TIMESTAMPDIFF(HOUR, cl.dt_insert, COALESCE (clh.dt_historico,CURRENT_TIMESTAMP())) < 72 THEN 6
 	    WHEN TIMESTAMPDIFF(HOUR, cl.dt_insert, COALESCE (clh.dt_historico, CURRENT_TIMESTAMP())) >= 72 THEN 7 end) as idx,
 		CASE
-	    WHEN TIMESTAMPDIFF(MINUTE, cl.dt_insert, COALESCE (clh.dt_historico, CURRENT_TIMESTAMP())) <= 30 THEN 'até 30 minutos'
-	    WHEN TIMESTAMPDIFF(MINUTE, cl.dt_insert, COALESCE (clh.dt_historico, CURRENT_TIMESTAMP())) > 30 AND TIMESTAMPDIFF(MINUTE, cl.dt_insert, clh.dt_historico) < 120 THEN 'entre_31 minutos e 2h'
-	    WHEN TIMESTAMPDIFF(HOUR, cl.dt_insert, COALESCE (clh.dt_historico,CURRENT_TIMESTAMP())) >= 2 AND TIMESTAMPDIFF(HOUR, cl.dt_insert, clh.dt_historico) < 8 THEN 'entre_2 e_8h'
-	    WHEN TIMESTAMPDIFF(HOUR, cl.dt_insert, COALESCE (clh.dt_historico, CURRENT_TIMESTAMP())) >= 8 AND TIMESTAMPDIFF(HOUR, cl.dt_insert, clh.dt_historico) < 24 THEN 'entre_8_24h'
-	    WHEN TIMESTAMPDIFF(HOUR, cl.dt_insert, COALESCE (clh.dt_historico, CURRENT_TIMESTAMP())) >= 24 AND TIMESTAMPDIFF(HOUR, cl.dt_insert, clh.dt_historico) < 48 THEN 'entre_24_48h'
-	    WHEN TIMESTAMPDIFF(HOUR, cl.dt_insert, COALESCE (clh.dt_historico, CURRENT_TIMESTAMP())) >= 48 AND TIMESTAMPDIFF(HOUR, cl.dt_insert, clh.dt_historico) < 72 THEN 'entre_48_72_h'
-	    WHEN TIMESTAMPDIFF(HOUR, cl.dt_insert, COALESCE (clh.dt_historico, CURRENT_TIMESTAMP())) >= 72 THEN 'mais_de_72_h'
+	    WHEN TIMESTAMPDIFF(MINUTE, cl.dt_insert, COALESCE (clh.dt_historico, CURRENT_TIMESTAMP())) <= 30 THEN 'Até 30 minutos'
+	    WHEN TIMESTAMPDIFF(MINUTE, cl.dt_insert, COALESCE (clh.dt_historico, CURRENT_TIMESTAMP())) > 30 AND TIMESTAMPDIFF(MINUTE, cl.dt_insert, COALESCE (clh.dt_historico,CURRENT_TIMESTAMP())) < 120 THEN 'De 31 minutos a 2 horas'
+	    WHEN TIMESTAMPDIFF(HOUR, cl.dt_insert, COALESCE (clh.dt_historico,CURRENT_TIMESTAMP())) >= 2 AND TIMESTAMPDIFF(HOUR, cl.dt_insert, COALESCE (clh.dt_historico,CURRENT_TIMESTAMP())) < 8 THEN 'De 2 a 8 horas'
+	    WHEN TIMESTAMPDIFF(HOUR, cl.dt_insert, COALESCE (clh.dt_historico, CURRENT_TIMESTAMP())) >= 8 AND TIMESTAMPDIFF(HOUR, cl.dt_insert, COALESCE (clh.dt_historico,CURRENT_TIMESTAMP())) < 24 THEN 'De 8 a 24 horas'
+	    WHEN TIMESTAMPDIFF(HOUR, cl.dt_insert, COALESCE (clh.dt_historico, CURRENT_TIMESTAMP())) >= 24 AND TIMESTAMPDIFF(HOUR, cl.dt_insert, COALESCE (clh.dt_historico,CURRENT_TIMESTAMP())) < 48 THEN 'De 24 a 48 horas'
+	    WHEN TIMESTAMPDIFF(HOUR, cl.dt_insert, COALESCE (clh.dt_historico, CURRENT_TIMESTAMP())) >= 48 AND TIMESTAMPDIFF(HOUR, cl.dt_insert, COALESCE (clh.dt_historico,CURRENT_TIMESTAMP())) < 72 THEN 'De 48 a 72 horas'
+	    WHEN TIMESTAMPDIFF(HOUR, cl.dt_insert, COALESCE (clh.dt_historico, CURRENT_TIMESTAMP())) >= 72 THEN 'Tempo superior a 72 horas'
 	END AS texto,
 	1 as qts_lead,
 	IF(COALESCE (cfe.ie_tipo_etapa, '0') not IN ('G', 'P'), 1, 0)as qtd_andamento,
@@ -30,7 +30,9 @@ FROM(
 	if(cfe.ie_tipo_etapa = 'G',1,0)as qtd_ganho
 	from crm_lead cl
 	left join crm_lead_historico clh on
-		clh.idcrm_lead = cl.idcrm_lead and cl.idcrm_funil_etapa = clh.idcrm_funil_etapa 
+		clh.idcrm_lead = cl.idcrm_lead and (cl.idcrm_funil_etapa = clh.idcrm_funil_etapa) 
+	left join crm_lead_historico clh2 on
+		clh2.idcrm_lead = cl.idcrm_lead and clh2.idcrm_funil_etapa is null
 	inner join crm_funil cf on
 		cf.idcrm_funil = cl.idcrm_funil
 	inner join crm_projeto cp on
@@ -41,6 +43,7 @@ FROM(
 		cp.idestabelecimento = :idestabelecimento
 		and cf.idcrm_funil = :idcrm_funil
 		and cl.status = 'A'
+		AND DATE(cl.dt_insert) BETWEEN :dt_inicial AND :dt_final		
 		GROUP BY cl.idcrm_lead)x
 GROUP BY x.idx,x.texto
 ORDER by x.idx;
