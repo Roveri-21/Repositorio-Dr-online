c8537fcba6a936e83dbe0a95244dd536193167dc
guilherme-dr-online <guilhermeinttegrar@gmail.com>
2023-11-29 13:59:51 -0300

    Query para tester se o trackeador_acao

---
+++ Guilherme/Querys/Tracker/Trackeador.sql
@@ -0,0 +1,5 @@
+SELECT * FROM trackeador_acao ta 
+order by 
+ta.idtrackeador_acao DESC;
+
+-- validação de acesso no site, caso seja necessario validar em algum site basta executar e esperar 10 segundos.
