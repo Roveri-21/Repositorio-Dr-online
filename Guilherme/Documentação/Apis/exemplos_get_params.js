8adb4b9fd5505420545341f11922c77167b175fe
guilherme-dr-online <guilhermeinttegrar@gmail.com>
2024-01-09 16:50:51 -0300

    exemplos de apis

---
+++ Guilherme/Documentação/Apis/exemplos_get_params.js
@@ -0,0 +1,35 @@
+const express = require('express'); //exporta o express
+const { Route } = require('react-router-dom');
+
+const server = express();
+
+// localhost:3000/curso
+server.get('/curso/:id', (req, res) => {
+    const id = req.params.id;
+    return res.json({ curso: `id ${id}`});
+})
+
+server.listen(3000);
+
+
+
+//outro exemplo
+
+
+const express = require('express'); //exporta o express
+const { Route } = require('react-router-dom');
+
+const server = express();
+const curso = ['Node Js', 'JavaScript','React']
+// localhost:3000/curso
+server.get('/curso/:index', (req, res) => {
+        const { index } = req.params
+    return res.json(curso[index]);
+})
+
+server.listen(3000);
+
+
+
+
+
