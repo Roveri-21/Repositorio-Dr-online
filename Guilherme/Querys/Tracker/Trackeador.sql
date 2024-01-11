df5bc09ca676bbc19759a47429ce6d3cdf79a240
guilherme-dr-online <guilhermeinttegrar@gmail.com>
2024-01-11 09:04:57 -0300

    Fitra pelo id do trackeador

--- Guilherme/Querys/Tracker/Trackeador.sql
+++ Guilherme/Querys/Tracker/Trackeador.sql
@@ -3,3 +3,10 @@ order by
 ta.idtrackeador_acao DESC;
 
 -- validação de acesso no site, caso seja necessario validar em algum site basta executar e esperar 10 segundos.
+
+
+SELECT * from trackeador_acao ta
+inner join trackeador_usuario tu on tu.idtrackeador_usuario = ta.idtrackeador_usuario and tu.idtrackeador = 12 -- altere o codigo do trackeador para acompanhar as novas requisições no site
+WHERE ta.dados is not NULL 
+order by ta.idtrackeador_acao  DESC;
+
