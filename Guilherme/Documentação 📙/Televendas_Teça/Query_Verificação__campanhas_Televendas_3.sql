34c9ca58bafc3d666ec5f5bfa81cc01143bbb5eb
guilherme-dr-online <guilhermeinttegrar@gmail.com>
2024-03-07 08:59:58 -0300

    Validações das televendas

--- Guilherme/Documentação 📙/Televendas_Teça/Query_Verificação__campanhas_Televendas_3.sql
+++ Guilherme/Documentação 📙/Televendas_Teça/Query_Verificação__campanhas_Televendas_3.sql
@@ -1,31 +1,43 @@
 -- VERIFICAR CAMPANHAS GERADAS
-    -- CAMPANHA 
-    SELECT 	* 
-    FROM 	TEL_CAMPANHA tc 
-    WHERE 	ID IN (501,502,503,504)
+    -- Traz as CAMPANHA GERADAS das semana
+    SELECT 	
+        * 
+    FROM
+     	TEL_CAMPANHA tc 
+    WHERE
+     	ID IN (561,562,563,564)
     ORDER BY ID DESC;
 
-    -- CLIENTES 
-    SELECT 	*
-    FROM 	TEL_CLIENTE tc 
---    WHERE ID_CAMPANHA IN (481,482,483,484,485,486)
-    WHERE ID_CAMPANHA IN (501,502,503,504);
+    -- Traz os CLIENTES  da campanha
+    SELECT
+     	*
+    FROM
+     	TEL_CLIENTE tc 
+    WHERE
+        ID_CAMPANHA IN (561,562,563,564);
     
-    -- PRODUTOS 
-    SELECT 	*
-    FROM 	TEL_CAMPANHA_PRODUTO_DESCONTO tcpd 
-    WHERE 	ID_CAMPANHA IN (501,502,503,504);
+    --Traz os PRODUTOS da campanha
+    SELECT
+     	*
+    FROM
+     	TEL_CAMPANHA_PRODUTO_DESCONTO tcpd 
+    WHERE
+     	ID_CAMPANHA IN (561,562,563,564);
 
     -- TELEFONES QUE SERÃO ENVIADOS
-    SELECT  * 
-    FROM    TEL_CLIENTE_TELEFONES_CAMPANHA
-    WHERE   ID_CAMPANHA IN (501,502,503,504);
+    SELECT
+      * 
+    FROM
+        TEL_CLIENTE_TELEFONES_CAMPANHA
+    WHERE   ID_CAMPANHA IN (561,562,563,564);
     
--- 
 -- ENVIO 
     -- SQL TELEVENDAS
-    SELECT *	
-    FROM TEL_ENVIO_TELEVENDAS tet 
-    WHERE tet."ds_email" IN (501,502,503,504)
+    SELECT
+        *	
+    FROM
+        TEL_ENVIO_TELEVENDAS tet 
+    WHERE
+     tet."ds_email" IN (561,562,563,564)
     -- NOTIFICAÇÃO: 145	Dep. Televendas - Envio de promoção positivação clientes
     
