e7bc2b94a09e2931681a0cf09159d6fe91680ae0
Guilherme <guilhermeinttegrar@gmail.com>
2024-08-19 11:31:40 -0300

    Footer das paginas

---
+++ webpets/my-app/src/Componentes/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+function Footer() {
+    return (
+        <div className='footer'>
+          <p>&copy; 2023 By Guilherme Roveri</p>
+        </div>
+      );
+  }
+  export default Footer;
