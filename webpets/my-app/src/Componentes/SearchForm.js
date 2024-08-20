ede0bf53119166ce4eec35c58966ebd185a17806
Guilherme <guilhermeinttegrar@gmail.com>
2024-08-20 17:46:21 -0300

    Searchform finalizado

--- webpets/my-app/src/Componentes/SearchForm.js
+++ webpets/my-app/src/Componentes/SearchForm.js
@@ -1,22 +1,28 @@
-import React, { useState, useEffect } from 'react';
 import './SearchForm.css';
 import InputCpfMask from './InputCpfMask'
+import MoneyInput from './MoneyInput'
+import InputNameMask from './InputNameMask'
 
 function SearchForm() {
     return (
         <div className='personal-data'>
             <fieldset id="formularioAdicionar">
-                <legend>Pesquisa</legend>
-                <label for="nomeVendedor">Nome Vendedor:</label>
-                <input type="text" id="nomeVendedor" placeholder="Nome Vendedor" />
+                <legend id="legendsearch">Pesquisa</legend>
+                <InputNameMask/>
                 <label for="nomeProduto">Nome Produto:</label>
                 <input type="text" id="nomeProduto" placeholder="Nome Produto" />
-                <label for="valorProduto">Valor Produto:</label>
-                <input type="text" id="valorProduto" placeholder="Valor Produto" />
+                <label for="MoneyInput">Valor Produto:</label>
+                <MoneyInput/>
                 <label for="dataVencimento">Data de Vencimento:</label>
                 <input type="date" id="dataVencimento" placeholder="Data de Vencimento" />
-                <InputCpfMask />
-                <button id="buttonAdicionar">Buscar</button>
+                <InputCpfMask/>
+                <label for="labelprod">Escolha o stado do Produto:</label>
+                <select name="selectstatusprod" id="selectstatusprod">
+                    <option value="Vencido">Vencido</option>
+                    <option value="Proxvencimento">Proximo do Vencimento</option>
+                    <option value="valido">Valido</option>
+                </select>
+                <button id="buttonsearch">Buscar</button>
             </fieldset>
         </div>
     )
