b3733f636fd84338b9b90f56795b0f69c83811bf
Guilherme <guilhermeinttegrar@gmail.com>
2024-08-22 09:22:21 -0300

    Visual finalizado para o botão search

--- webpets/my-app/src/Componentes/SearchForm.js
+++ webpets/my-app/src/Componentes/SearchForm.js
@@ -6,25 +6,39 @@ import InputNameMask from './InputNameMask'
 function SearchForm() {
     return (
         <div className='personal-data'>
-            <fieldset id="formularioAdicionar">
-                <legend id="legendsearch">Pesquisa</legend>
-                <InputNameMask/>
-                <label for="nomeProduto">Nome Produto:</label>
-                <input type="text" id="nomeProduto" placeholder="Nome Produto" />
-                <label for="MoneyInput">Valor Produto:</label>
-                <MoneyInput/>
-                <label for="dataVencimento">Data de Vencimento:</label>
-                <input type="date" id="dataVencimento" placeholder="Data de Vencimento" />
-                <InputCpfMask/>
-                <label for="labelprod">Escolha o stado do Produto:</label>
-                <select name="selectstatusprod" id="selectstatusprod">
-                    <option value="Vencido">Vencido</option>
-                    <option value="Proxvencimento">Proximo do Vencimento</option>
-                    <option value="valido">Valido</option>
-                </select>
-                <button id="buttonsearch">Buscar</button>
-            </fieldset>
-        </div>
+        <fieldset id="formularioAdicionar">
+            <legend id="legendsearch">Pesquisa</legend>
+            <div className="field-group">
+                <div>
+                    <label for="nomeProduto">Nome Produto:</label>
+                    <input type="text" id="nomeProduto" placeholder="Nome Produto" />
+                </div>
+                <div>
+                    <label for="MoneyInput">Valor Produto:</label>
+                    <MoneyInput />
+                </div>
+                <div>
+                    <label for="dataVencimento">Data de Vencimento:</label>
+                    <input type="date" id="dataVencimento" />
+                </div>
+                <div>
+                    <InputCpfMask />
+                </div>
+                <div>
+                    <label for="selectstatusprod">Escolha o estado do Produto:</label>
+                    <select id="selectstatusprod">
+                        <option value="Vencido">Vencido</option>
+                        <option value="Proxvencimento">Proximo do Vencimento</option>
+                        <option value="valido">Valido</option>
+                    </select>
+                </div>
+                <div>
+                    <button id="buttonsearch">Buscar</button>
+                </div>
+            </div>
+        </fieldset>
+    </div>
+    
     )
 }
 export default SearchForm;
