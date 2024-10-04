21dbec9cf558323ce176d163b8d079bc66f041c3
Roveri-21 <guilherroveri21@gmail.com>
2024-10-04 16:37:47 -0300

    logica de nageção de arquivos

--- webpets/my-app/src/App.js
+++ webpets/my-app/src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
-import React, { useState, useEffect } from 'react';
-import Menu from './Componentes/Menu'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Menu from './Componentes/Menu';
+import Estoque from './Componentes/Estoque';
+//import Admin from './Componentes/Admin'; // Certifique-se de importar os componentes corretamente
+//import Logs from './Componentes/Logs';
+//import Login from './Componentes/Login';
+
 function App() {
   return (
-<Menu/>
-
-  )
+    <Router>
+      <Menu />
+      <Routes>
+        <Route path="/estoque" element={<Estoque />} />
+        {/* <Route path="/admin" element={<Admin />} />
+        <Route path="/logs" element={<Logs />} />
+        <Route path="/login" element={<Login />} /> */}
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
